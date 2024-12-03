37547330171dbcb6f84f13ad4d97abd3c57cddab
Yifei Sun <ysun@hey.com>
2024-12-03 00:33:22 -0500

    2024-3

--- src/year_2024/day_3.ml
+++ src/year_2024/day_3.ml
@@ -1,4 +1,8 @@
-open Core
+open Common
 
-let part_1 input = String.concat ~sep:" " input
-let part_2 input = String.concat ~sep:" " input
+let part_1 input =
+  let txt = get_string input in
+  txt
+;;
+
+let part_2 input = input
