3752af2dd118ae159c9da52b4b521e516744b035
Yifei Sun <ysun@hey.com>
2024-12-03 17:14:27 -0500

    2024-2

--- lib/common.ml
+++ lib/common.ml
@@ -19,17 +19,48 @@ let get_lines path =
   | _ -> []
 ;;
 
+(* space separated string to a list of strings *)
+(* String *)
+let of_string_list s =
+  String.split_on_chars s ~on:[ ' ' ] |> List.filter ~f:(fun x -> String.length x > 0)
+;;
+
+(* space separated string to a list of ints *)
+(* String *)
+let of_int_list s = of_string_list s |> List.map ~f:Int.of_string
+
+(* space separated string to a pair of ints (strict two column) *)
+(* String *)
 let of_int_pair s =
-  String.split_on_chars s ~on:[ ' ' ]
-  |> List.filter ~f:(fun x -> String.length x > 0)
-  |> List.map ~f:Int.of_string
+  of_int_list s
   |> function
   | [ a; b ] -> a, b
   | _ -> failwith "invalid format"
 ;;
 
+(* sort string list with two columns and convert to in pairs *)
+(* e.g. ["2 4"; "1 2 "] -> [1; 2], [2; 4] *)
+(* List *)
 let of_sorted_two_column_int_list input =
   List.map ~f:of_int_pair input
   |> List.unzip
   |> fun (a, b) -> List.sort a ~compare:Int.compare, List.sort b ~compare:Int.compare
+;;
+
+(* check if given list is in increasing or decreasing order, with additional constraint between elements *)
+(* List *)
+let is_sorted_custom list ~compare =
+  match list with
+  | [] | [ _ ] -> true
+  | hd :: tl ->
+    let rec aux prev incr decr = function
+      | [] -> incr || decr
+      | x :: xs ->
+        aux
+          x
+          (compare prev x && incr && prev <= x)
+          (compare prev x && decr && prev >= x)
+          xs
+    in
+    aux hd true true tl
 ;;
--- src/year_2024/day_2.ml
+++ src/year_2024/day_2.ml
@@ -1,2 +1,29 @@
-let part_1 input = input
-let part_2 input = input
+open Common
+open Core
+
+let check x =
+  is_sorted_custom x ~compare:(fun a b ->
+    let diff = abs (a - b) in
+    diff >= 1 && diff <= 3)
+;;
+
+let part_1 path =
+  get_lines path
+  |> List.map ~f:of_int_list
+  |> List.filter ~f:check
+  |> List.length
+  |> string_of_int
+;;
+
+let part_2 path =
+  let valid x =
+    List.existsi x ~f:(fun i _ ->
+      let l' = List.filteri x ~f:(fun idx _ -> i <> idx) in
+      check l')
+  in
+  get_lines path
+  |> List.map ~f:of_int_list
+  |> List.filter ~f:valid
+  |> List.length
+  |> string_of_int
+;;
